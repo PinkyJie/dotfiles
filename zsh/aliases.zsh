70489fbb65c6ce561184a5661fa07273880bbc1e
PinkyJie <pilixiaoxuanfeng@gmail.com>
2016-02-18 22:48:58 +0800

    cnpm alias added

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -265,3 +265,9 @@ alias kthxbai='halt'
 
 
 alias gfw='proxychains4'
+
+# alias for cnpm
+alias cnpm="npm --registry=https://registry.npm.taobao.org \
+--cache=$HOME/.npm/.cache/cnpm \
+--disturl=https://npm.taobao.org/dist \
+--userconfig=$HOME/.cnpmrc"
