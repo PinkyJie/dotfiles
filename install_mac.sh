15e882d3a4fa1a9f2e66ee0e8e92fe02a878b27e
Wenbo Jie <pinkyjie.gn@gmail.com>
2023-04-02 14:14:55 +1000

    Add mising files

---
+++ install_mac.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+source ./_utils.sh
+
+print_header "----------"
+print_header "This script will help you setup your Mac development environment."
+print_header "Make sure you have already installed Xcode tool: xcode-select --install"
+print_header "Press Enter to continue."
+print_header "----------"
+
+read user_action
+if [ ! -z $user_action ]; then
+  print_header "-- Exit!"
+  exit 0
+fi
+
+print_header "1. Install Homebrew"
+# homebrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+check_error "homebrew"
+
+# cli tools
+print_header "-- Homebrew CLI tools"
+brew install git diff-so-fancy tig nvm fzf zsh-autosuggestions
+check_error "homebrew cli tools"
+
+# fzf keybindings
+print_header "-- fzf keybindings"
+$(brew --prefix)/opt/fzf/install
+
+# apps
+print_header "-- Homebrew apps"
+brew install --cask warp iterm2 visual-studio-code google-chrome appcleaner iina
+check_error "homebrew apps"
+
+print_header "2. Oh My Zsh"
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+check_error "oh-my-zsh"
+
+# zsh theme
+print_header "-- PowerLevel10k theme"
+brew install romkatv/powerlevel10k/powerlevel10k
+check_error "zsh theme"
+
+print_header "3. SSH keygen"
+ssh-keygen -t rsa -b 4096 -C pinkyjie.gn@gmail.com
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+print_header "4. Config soft link"
+
+# zsh
+rm -f ~/.zsh ~/.zhsrc
+ln -s ~/.dotfiles/zsh ~/.zsh
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+
+# ssh
+ln -s ~/.dotfiles/ssh/config ~/.ssh/config
+
+# git
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+
+# run VSCode once to generate User folder
+code &
+vscode_pid=$!
+sleep 1s
+kill -9 $vscode_pid
+
+# vscode
+rm -rf ~/Library/Application\ Support/Code/User
+ln -s ~/.dotfiles/vscode/User ~/Library/Application\ Support/Code/User
+print_header "-- Install vscode plugins"
+cat ~/.dotfiles/vscode/extension.list | xargs -L 1 code --install-extension
+
+print_header "\n\nAll Done!"
---
+++ install_ubuntu.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+source ./_utils.sh
+
+print_header "----------"
+print_header "This script will help you setup your Ubuntu development environment."
+print_header "Press Enter to continue."
+print_header "----------"
+
+read user_action
+if [ ! -z $user_action ]; then
+  print_header "-- Exit!"
+  exit 0
+fi
+
+print_header "0. Apt update"
+sudo apt update
+
+print_header "1. Config soft link"
+
+# zsh
+rm -f ~/.zsh ~/.zhsrc
+ln -s ~/.dotfiles/zsh ~/.zsh
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+
+# git
+ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+
+print_header "2. Install cli tools"
+# cli tools
+sudo apt -y install tig fzf zsh zsh-autosuggestions
+check_error "cli tools"
+
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+print_header "3. Oh My Zsh"
+# oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+check_error "oh-my-zsh"
+
+# zsh theme
+print_header "-- PowerLevel10k theme"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+check_error "zsh theme"
+
+print_header "4. SSH keygen"
+ssh-keygen -t rsa -b 4096 -C pinkyjie.gn@gmail.com
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_rsa
+
+print_header "\n\nAll Done!"
