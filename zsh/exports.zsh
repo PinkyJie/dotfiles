dce7c93e2ffc6d568ad5077a8e4bcb71e3776b14
Wenbo Jie <pinkyjie.gn@gmail.com>
2024-03-31 22:07:42 +1100

    add bitwarden

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,2 +1,2 @@
 # Flutter
-export PATH="$PATH:${HOME}/tools/flutter/bin:${HOME}/Library/Python/3.8/bin"
+export PATH="$PATH:${HOME}/tools/flutter/bin:${HOME}/Library/Python/3.8/bin:${HOME}/Library/Android/sdk/platform-tools/"
